2e78bac9101351be0475051a9d6413be81352d97
James Ristau <jamesristau@msn.com>
2015-02-27 16:20:07 -0500

    My birthday project

--- Gemfile
+++ Gemfile
@@ -3,3 +3,6 @@ source 'https://rubygems.org'
 
 
 gem 'sinatra', '~> 1.4.5'
+gem 'rack', '1.5.2'
+gem 'cat_api'
+gem 'holidapi'
--- app.rb
+++ app.rb
@@ -1,10 +1,13 @@
 # app.rb
 require 'sinatra'
+require 'cat_api'
+require "holidapi"
 
-class MyWebApp < Sinatra::Base
+class BirthdayHolidays < Sinatra::Base
 	get '/' do 
-		last_modified Time.now
-		send_file "views/index#{ rand(3) + 1 }.html"
+		@birthday_holidays = HolidApi.get(country: 'us', year: 1992, month: 12).flatten
+		@current_holidays = HolidApi.get(country: 'us', year: Time.now.year, month: Time.now.month).flatten	
+		erb :index
 
 	end
 end
